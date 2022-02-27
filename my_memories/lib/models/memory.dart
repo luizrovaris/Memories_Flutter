208fd782eed3942530b6bcef9392397057deb588
luizrovaris <luiz_rovaris@hotmail.com>
2022-02-27 21:41:26 +0000

    Adding flutter project

---
+++ my_memories/lib/models/memory.dart
@@ -0,0 +1,17 @@
+class Memory {
+  int? id;
+  String? image;
+  double? latitude;
+  double? longitude;
+  String? description;
+  String? formatedAddress;
+
+  Memory(
+    this.id,
+    this.image,
+    this.latitude,
+    this.longitude,
+    this.description,
+    this.formatedAddress,
+  );
+}
